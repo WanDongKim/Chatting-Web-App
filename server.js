a4e103929be5bc03c5d97c730909a19c232e3501
unknown <zeumes12@gmail.com>
2017-11-08 12:36:50 -0500

    Connected Mongo DB

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb://user:1234@ds255265.mlab.com:55265/learning-node1130'
+
 var messages = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'Hello'}
@@ -25,6 +29,10 @@ app.post('/messages', (req, res) =>{
 
 io.on('Connection', (socket) =>{
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl,{useMongoClient: true },(err) =>{
+    console.log('mongo db connection',err)
 })
 
 var server = http.listen(3000, ()=>{
