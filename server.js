25a79a08601484bd702b61e01a295aaf17937cc0
unknown <zeumes12@gmail.com>
2017-11-08 13:25:36 -0500

    Added jasmine for testing

--- server.js
+++ server.js
@@ -31,19 +31,28 @@ app.get('/messages', (req, res) =>{
 })
 
 app.post('/messages', async (req, res) =>{
-    var message = new Message(req.body)
-    var savedMessage = await message.save()
-    
-    console.log('saved')
-    
-    var censored =  await Message.findOne({message: 'badword'})
-    
-    if(censored) 
-           await Message.remove({_id: censored.id})
-    else
-        io.emit('message',req.body)
+
+    try{
+        var message = new Message(req.body)
+        var savedMessage = await message.save()
         
-    res.sendStatus(200)
+        console.log('saved')
+        
+        var censored =  await Message.findOne({message: 'badword'})
+        
+        if(censored) 
+               await Message.remove({_id: censored.id})
+        else
+            io.emit('message',req.body)
+            
+        res.sendStatus(200)
+    }catch(error){
+        res.sendStatus(500)
+        return console.error(error)
+    } finally{
+        console.log('message post called')
+    }
+
     // .catch((err)=>{
     //     res.sendStatus(500)
     //     return console.error(err)
